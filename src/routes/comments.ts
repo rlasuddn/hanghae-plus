df86857b66a96edf459ef71419f80612f6ebcbb4
rlasuddn <ywaves073@gmail.com>
2024-11-30 21:49:35 +0900

    Feat: 댓글 기능 구현

--- src/routes/comments.ts
+++ src/routes/comments.ts
@@ -1,22 +1,96 @@
-import { Router, Request, Response } from "express"
+import { Router, Request, Response, NextFunction } from "express"
+import prisma from "../../prisma/index"
 
 const router = Router()
 
-router.get("/:postId", (req: Request, res: Response) => {
-    console.log("aa")
-    res.json("댓글 목록 조회")
+router.get("/:postId", async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { postId } = req.params
+        const comments = await prisma.comments.findMany({
+            where: {
+                postId: Number(postId),
+            },
+            select: {
+                content: true,
+                author: {
+                    select: {
+                        name: true,
+                    },
+                },
+            },
+            take: 10,
+            orderBy: {
+                createdAt: "desc",
+            },
+        })
+        res.json(comments)
+    } catch (err) {
+        next(err)
+    }
 })
 
-router.post("/:postId", (req: Request, res: Response) => {
-    res.json("댓글 작성")
+router.post("/:postId/:authorId", async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { content } = req.body
+        const { postId, authorId } = req.params
+
+        if (!content) {
+            throw new Error("댓글 내용을 입력해주세요")
+        }
+
+        const create = await prisma.comments.create({
+            data: {
+                content,
+                author: {
+                    connect: { id: Number(authorId) },
+                },
+                post: {
+                    connect: { id: Number(postId) },
+                },
+            },
+        })
+        res.json(create)
+    } catch (err) {
+        next(err)
+    }
 })
 
-router.patch("/:commentsId", (req: Request, res: Response) => {
-    res.json("댓글 수정")
+router.patch("/:commentsId", async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { content } = req.body
+        const { commentsId } = req.params
+
+        if (!content) {
+            throw new Error("댓글 내용을 입력해주세요")
+        }
+
+        const update = await prisma.comments.update({
+            where: {
+                id: Number(commentsId),
+            },
+            data: {
+                content,
+            },
+        })
+
+        res.json(update)
+    } catch (err) {
+        next(err)
+    }
 })
 
-router.delete("/:commentsId", (req: Request, res: Response) => {
-    res.json("댓글 삭제")
+router.delete("/:commentsId", async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { commentsId } = req.params
+
+        const deleteRow = await prisma.comments.delete({
+            where: { id: Number(commentsId) },
+        })
+
+        res.json(deleteRow)
+    } catch (err) {
+        next(err)
+    }
 })
 
 export default router
